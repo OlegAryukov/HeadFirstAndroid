67254b39067c1b03ce7f2543d0a45ab0be6d26a6
olega <oleg.aryukov@ediweb.com>
2017-03-16 19:11:30 +0300

    next step in android

---
+++ app/src/main/java/aryukov/ru/lesson1/ReceieveMessageActivity.java
@@ -0,0 +1,19 @@
+package aryukov.ru.lesson1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ReceieveMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receieve_message);
+        Intent intent = getIntent();
+        String messageTaext = intent.getStringExtra(TEXT_SERVICES_MANAGER_SERVICE);
+        TextView messageView = (TextView) findViewById(R.id.messageForSend);
+        messageView.setText(messageTaext);
+    }
+}
